032d3bcded81a1ca13e1b092bcb7a04cdee05f6a
Bertrand Retif <bertrand@sudokeys.com>
2019-06-12 08:46:21 +0200

    config in pentaho-solutions

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,16 +1,16 @@
 touch .install_plugin
 
-if [ ! -f ".pentaho_pgconfig" ]; then
+if [ ! -f "$PENTAHO_HOME/pentaho-server/pentaho-solutions/.pentaho_pgconfig" ]; then
    bash -x $PENTAHO_HOME/scripts/setup_postgresql.sh
    #HOSTNAME=$(`echo hostname`)
 
 #   sed -i "s/node1/${HOSTNAME}/g" $PENTAHO_HOME/pentaho-server/pentaho-solutions/system/jackrabbit/repository.xml
-   touch .pentaho_pgconfig
+   touch $PENTAHO_HOME/pentaho-server/pentaho-solutions/.pentaho_pgconfig
 fi
 
-if [ ! -f ".install_plugin" ]; then
+if [ ! -f "$PENTAHO_HOME/pentaho-server/pentaho-solutions/.install_plugin" ]; then
    sh $PENTAHO_HOME/scripts/install_plugin.sh
-   touch .install_plugin
+   touch $PENTAHO_HOME/pentaho-server/pentaho-solutions/.install_plugin
 fi
 
 if [ -f "./custom_script.sh" ]; then
